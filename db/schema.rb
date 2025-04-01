3a48f0678dc062c6a5848a845217a6fcfbe27dc6
notihait <gr.maria09@gmail.com>
2025-04-01 19:37:22 +0300

    it works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_27_131512) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_163427) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -51,7 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_27_131512) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name", null: false
-    t.string "text"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
